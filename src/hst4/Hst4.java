46580b7ea3d047cb9e0e793cd9032fa47160af0c
timodokter <timo.dokter@student.rocvf.nl>
2020-09-07 14:04:11 +0100

    update 4.3

--- src/hst4/Hst4.java
+++ src/hst4/Hst4.java
@@ -10,12 +10,14 @@ public class Hst4  extends Applet {
 
     public void paint(Graphics g) {
         setBackground(Color.black);
+        //dit is de gelijkbenige driehoek
         g.setColor(Color.blue);
         g.drawLine(50,120,100,70);
         g.drawLine(100,70,150,120);
         g.drawLine(50,120,150,120);
 
+        //dit is het huisje
         g.setColor(Color.GREEN);
-        g.drawRect();
+        g.drawRect(170,70,100,80);
     }
 }
