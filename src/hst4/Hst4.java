7e0a2ab1bf5082f054624b08fdad99501ce0112a
timodokter <timo.dokter@student.rocvf.nl>
2020-09-07 13:43:45 +0100

    update 4.1

---
+++ src/hst4/Hst4.java
@@ -0,0 +1,16 @@
+package hst4;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Hst4  extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        setBackground(Color.black);
+        g.setColor(Color.blue);
+        g.drawLine();
+    }
+}
