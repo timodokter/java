dfc3c9a74cefdfaed07b34bc9ea314da0b0967eb
timodokter <timo.dokter@student.rocvf.nl>
2020-09-08 10:56:02 +0100

    update 5.2

--- src/hst5/Opdracht1.java
+++ src/hst5/Opdracht1.java
@@ -7,18 +7,24 @@ public class Opdracht1 extends Applet {
     //decleratie
     Color opvulkleur;
     Color lijnkleur;
+    Color achtergrond;
     int breedte;
     int hoogte;
+    int breedte2;
 
     public void init() {
         //initialisatie.
         opvulkleur = Color.MAGENTA;
         lijnkleur = Color.black;
+        achtergrond = Color.GRAY;
         breedte = 200;
         hoogte = 100;
+        breedte2 = 100;
     }
 
     public void paint(Graphics g) {
+
+        setBackground(achtergrond);
 
         //Rechthoek
         g.setColor(lijnkleur);
@@ -46,10 +52,23 @@ public class Opdracht1 extends Applet {
 
         //Taartpunt
         g.setColor(lijnkleur);
-        g.drawArc();
+        g.drawArc(520,100,breedte,hoogte,0,360);
+        g.setColor(opvulkleur);
+        g.fillArc(520,100,breedte,hoogte,0,45);
 
         //Cirkel
+        g.setColor(lijnkleur);
+        g.drawArc(520,225,breedte2,hoogte,0,360);
 
+        //teksten
+        g.setColor(lijnkleur);
+        g.drawString("Lijn",100,95);
+        g.drawString("Rechthoek",15,25);
+        g.drawString("Afgeronde rechhoek",15,75);
+        g.drawString("Gevulde rechthoek met ovaal",125,20);
+        g.drawString("Gevulde ovaal",125,80);
+        g.drawString("Taartpunt met ovaal eromheen",235,25);
+        g.drawString("Cirkel",235,70);
 
     }
 }
