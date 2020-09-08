f7567aa5abc94adedff56124ad88c9be719c479b
timodokter <timo.dokter@student.rocvf.nl>
2020-09-08 11:25:47 +0100

    update 5.4

---
+++ src/hst5/Opdracht2.java
@@ -0,0 +1,28 @@
+package hst5;
+
+import java.applet.*;
+import java.awt.*;
+
+public class Opdracht2 extends Applet{
+
+    //decleratie
+    Color lijnkleur;
+    Color opvulkleurV;
+    Color opvulkleurJ;
+    Color opvulkleurH;
+    Color achtergrond;
+    int hoogteV;
+    int hoogteJ;
+    int hoogteH;
+
+    public void init() {
+     //initialisatie
+     lijnkleur = Color.blue;
+     opvulkleurV = Color.CYAN;
+     opvulkleurJ = Color.red;
+     opvulkleurH = Color.orange;
+     achtergrond = Color.BLACK;
+     hoogteV = 
+
+    }
+}
