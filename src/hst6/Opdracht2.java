7e64a79aa2f2a3dda4ec285b3dd06df62915a107
timodokter <timo.dokter@student.rocvf.nl>
2020-09-09 09:48:59 +0100

    update 6.2

---
+++ src/hst6/Opdracht2.java
@@ -0,0 +1,39 @@
+package hst6;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht2 extends Applet {
+    //decleratie
+    Color achtergrondkleur;
+    Color tekstkleur;
+    double uitkomstjaar;
+    double uitkomstdag;
+    double uitkomstuur;
+    double uitkomstminuut;
+    int secperminuut;
+    int secperuur;
+    int secperdag;
+    int secperjaar;
+
+    public void init() {
+        achtergrondkleur = Color.black;
+        tekstkleur = Color.white;
+        secperminuut = 60;
+        secperuur = 60 * secperminuut;
+        secperdag = 24 * secperuur;
+        secperjaar = 365 * secperdag;
+        uitkomstminuut = secperminuut;
+        uitkomstuur = secperuur;
+        uitkomstdag = secperdag;
+        uitkomstjaar = secperjaar;
+    }
+
+    public void paint(Graphics g) {
+        setBackground(achtergrondkleur);
+        g.setColor(tekstkleur);
+        g.drawString("Het aantal seconden per uur is: " + uitkomstuur,20,20);
+        g.drawString("Het aantal seconden per dag is: " + uitkomstdag,20,35);
+        g.drawString("Het aantal seconden per jaar is: " + uitkomstjaar,20,50);
+    }
+}
