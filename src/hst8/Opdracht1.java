5d13eba24924191396bb9e3c53d68658214110c9
timodokter <timo.dokter@student.rocvf.nl>
2020-09-09 15:48:49 +0100

    update 8.1

--- src/hst8/Opdracht1.java
+++ src/hst8/Opdracht1.java
@@ -1,4 +1,29 @@
 package hst8;
 
-public class Opdracht1 {
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+
+public class Opdracht1 extends Applet {
+    Button knop;
+    String schermtekst;
+
+    public void init() {
+        schermtekst = "Doet deze knop wel iets?";
+        knop = new Button("Klik op mij");
+        KnopListener kl = new KnopListener();
+        knop.addActionListener( kl );
+        add(knop);
+    }
+
+    public void paint(Graphics g) {
+        g.drawString(schermtekst, 50, 60 );
+    }
+
+    class KnopListener implements ActionListener {
+        public void actionPerformed( ActionEvent e ) {
+            schermtekst = "Ja, deze knop doet wel iets";
+        }
+    }
 }
