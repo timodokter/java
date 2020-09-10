f82f2076001d8a32be358581c6f8e16b4de6cc04
timodokter <timo.dokter@student.rocvf.nl>
2020-09-10 09:23:45 +0100

    update 8.4

--- src/hst8/Opdracht1.java
+++ src/hst8/Opdracht1.java
@@ -6,30 +6,41 @@ import java.awt.event.*;
 
 
 public class Opdracht1 extends Applet {
-    Button knopoké;
+    Button knopoke;
     Button knopreset;
     Label label;
     TextField tekstvak;
+    String s;
 
     public void init() {
         tekstvak = new TextField("klik op mij");
-        knopoké = new Button("oké");
+        knopoke = new Button("oké");
         knopreset = new Button("reset");
-        label = new Label("type iets in het tekstvakje");
-        knopreset.addActionListener(new Knoplistener() );
+        label = new Label("type uw gebruikers naam in het tekstvak en druk dan op oké");
+        knopreset.addActionListener(new Knoplistenerreset() );
+        knopoke.addActionListener(new Knoplisteneroke() );
         add(label);
         add(tekstvak);
-        add(knopoké);
+        add(knopoke);
         add(knopreset);
+        s = "";
     }
 
     public void paint(Graphics g) {
-        g.drawString("type een hele lange tekst " + "in het tekstvakje" + "en klik op oké",50,60 );
+        g.drawString("Gebruikersnaam: ",50,70 );
+        g.drawString(s,150,70);
     }
 
-    class Knoplistener implements ActionListener{
+    class Knoplistenerreset implements ActionListener {
         public void actionPerformed( ActionEvent e ) {
             tekstvak.setText("");
+            repaint();
+        }
+    }
+
+    class Knoplisteneroke implements ActionListener {
+        public void actionPerformed( ActionEvent e ) {
+            s = tekstvak.getText();
             repaint();
         }
     }
