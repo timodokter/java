e451fe9c6929cfb853d163952edff519aae1b443
timodokter <timo.dokter@student.rocvf.nl>
2020-09-11 09:10:24 +0100

    update 8.11

--- src/hst8/Opdracht3.java
+++ src/hst8/Opdracht3.java
@@ -7,5 +7,40 @@ import java.awt.event.*;
 public class Opdracht3 extends Applet{
 
     //decleratie
+    Label label;
+    TextField tekstvak;
+    Button oke;
+    String s;
+    double bedrag;
 
+    public void init() {
+        //initialisatie
+        bedrag = 0;
+        label = new Label("type het bedrag waarbij u 21% BTW wilt optellen hiernaast in het tekstvak");
+        tekstvak = new TextField("type hier het bedrag",15);
+        oke = new Button("oké");
+        s = "";
+
+        //initialisatieknoppen
+        oke.addActionListener(new knoplisteneroke());
+
+        //initialisatie add's
+        add(label);
+        add(tekstvak);
+        add(oke);
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("bedrag inc. BTW: ",20,80);
+        g.drawString(String.valueOf(bedrag),115,80);
+    }
+
+    private class knoplisteneroke implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+        s = tekstvak.getText();
+        bedrag = Double.parseDouble(s);
+        bedrag *= 1.21;
+        repaint();
+        }
+    }
 }
