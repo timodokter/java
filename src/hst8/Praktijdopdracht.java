384e5ee27229a646201ba5150af5fd2e3c64f6d0
timodokter <timo.dokter@student.rocvf.nl>
2020-09-11 09:37:49 +0100

    update 8.13

--- src/hst8/Praktijdopdracht.java
+++ src/hst8/Praktijdopdracht.java
@@ -2,6 +2,8 @@ package hst8;
 
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class Praktijdopdracht extends Applet{
@@ -12,9 +14,15 @@ public class Praktijdopdracht extends Applet{
     Button min;
     Button keer;
     Button delen;
+    double t1;
+    double t2;
+    double uitkomst;
+    String suitkomst;
 
     public void init() {
         //initialisatie tekst
+        t1 = 0.0;
+        t2 = 0.0;
         tekstvak1 = new TextField("", 5);
         tekstvak2 = new TextField("", 5);
 
@@ -22,10 +30,67 @@ public class Praktijdopdracht extends Applet{
         plus = new Button("+");
         min = new Button("-");
         keer = new Button("*");
-        delen = new Button
+        delen = new Button("/");
+        plus.addActionListener(new knoplistenerplus());
+        min.addActionListener(new knoplistenermin());
+        keer.addActionListener(new knoplistenerkeer());
+        delen.addActionListener(new knoplistenerdelen());
 
         //initialisatie add
         add(tekstvak1);
         add(tekstvak2);
+        add(plus);
+        add(min);
+        add(keer);
+        add(delen);
+    }
+
+
+    private class knoplistenerplus implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            t1 = Double.parseDouble(tekstvak1.getText());
+            t2 = Double.parseDouble(tekstvak2.getText());
+            uitkomst = t1 + t2;
+            suitkomst = String.valueOf(uitkomst);
+            tekstvak1.setText(String.valueOf(uitkomst));
+            tekstvak2.setText("");
+        }
+    }
+
+    private class knoplistenermin implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            t1 = Double.parseDouble(tekstvak1.getText());
+            t2 = Double.parseDouble(tekstvak2.getText());
+            uitkomst = t1 - t2;
+            suitkomst = String.valueOf(uitkomst);
+            tekstvak1.setText(String.valueOf(uitkomst));
+            tekstvak2.setText("");
+        }
+    }
+
+    private class knoplistenerkeer implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            t1 = Double.parseDouble(tekstvak1.getText());
+            t2 = Double.parseDouble(tekstvak2.getText());
+            uitkomst = t1 * t2;
+            suitkomst = String.valueOf(uitkomst);
+            tekstvak1.setText(String.valueOf(uitkomst));
+            tekstvak2.setText("");
+        }
+    }
+
+    private class knoplistenerdelen implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            t1 = Double.parseDouble(tekstvak1.getText());
+            t2 = Double.parseDouble(tekstvak2.getText());
+            uitkomst = t1 / t2;
+            suitkomst = String.valueOf(uitkomst);
+            tekstvak1.setText(String.valueOf(uitkomst));
+            tekstvak2.setText("");
+        }
     }
 }
