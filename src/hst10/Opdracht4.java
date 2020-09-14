297563b266ae3b4ea1a3ea7157d9c21259a698ca
timodokter <timo.dokter@student.rocvf.nl>
2020-09-14 09:43:59 +0100

    update 10.10

--- src/hst10/Opdracht4.java
+++ src/hst10/Opdracht4.java
@@ -1,5 +1,6 @@
 package hst10;
 
+import javax.xml.soap.Text;
 import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -10,6 +11,7 @@ public class Opdracht4 extends Applet {
 
     Label labelmaand;
     TextField tmaand;
+    TextField tjaar;
     String sjaar;
     String smaand;
     String sdagen;
@@ -22,12 +24,15 @@ public class Opdracht4 extends Applet {
         setSize(800, 400);
         antwoord = "";
         smaand = "dit is geen geldig nummer";
-        labelmaand = new Label("type een maand nummer in het tekstvak en druk op enter.");
+        labelmaand = new Label("type een maand nummer in het tekstvak en druk op enter. op deze manier jaartal links en maand rechts zo dus: 2020 1");
         tmaand = new TextField("",5);
+        tjaar = new TextField("",5);
+        tjaar.addActionListener(new textlistenertmaand());
         tmaand.addActionListener(new textlistenertmaand());
 
         //initialisatie add's
         add(labelmaand);
+        add(tjaar);
         add(tmaand);
     }
 
@@ -38,66 +43,68 @@ public class Opdracht4 extends Applet {
     private class textlistenertmaand implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             smaand = tmaand.getText();
+            sjaar = tjaar.getText();
             dmaand = Double.parseDouble(smaand);
+            djaar = Double.parseDouble(sjaar);
             if (dmaand == 1) {
                 smaand = "Januari";
                 sdagen = "31";
-                antwoord = "dit is het jaartal en de maand die bij uw nummer hoort en hoeveel dagen deze maand heeft: " + smaand + " en " + sdagen + " dagen.";
+                antwoord = "dit is het jaartal en de maand die bij uw nummers hoort en hoeveel dagen deze maand dan heeft: " + smaand + " " + sjaar + " en " + sdagen + " dagen.";
             }
-            if (dmaand == 2) {
+            if ( (djaar % 4 == 0 && !(djaar % 100 == 0)) || djaar % 400 == 0 && dmaand == 2)  {
                 smaand = "Februari";
-                sdagen = "28 of 29";
-                antwoord = "dit is het jaartal en de maand die bij uw nummer hoort en hoeveel dagen deze maand heeft: " + smaand + " en " + sdagen + " dagen.";
+                sdagen = "29";
+                antwoord = "dit is het jaartal en de maand die bij uw nummers hoort en hoeveel dagen deze maand dan heeft: " + smaand + " " + sjaar + " en " + sdagen + " dagen.";
             }
             if (dmaand == 3) {
                 smaand = "Maart";
                 sdagen = "31";
-                antwoord = "dit is het jaartal en de maand die bij uw nummer hoort en hoeveel dagen deze maand heeft: " + smaand + " en " + sdagen + " dagen.";
+                antwoord = "dit is het jaartal en de maand die bij uw nummers hoort en hoeveel dagen deze maand dan heeft: " + smaand + " " + sjaar + " en " + sdagen + " dagen.";
             }
             if (dmaand == 4) {
                 smaand = "April";
                 sdagen = "30";
-                antwoord = "dit is het jaartal en de maand die bij uw nummer hoort en hoeveel dagen deze maand heeft: " + smaand + " en " + sdagen + " dagen.";
+                antwoord = "dit is het jaartal en de maand die bij uw nummers hoort en hoeveel dagen deze maand dan heeft: " + smaand + " " + sjaar + " en " + sdagen + " dagen.";
             }
             if (dmaand == 5) {
                 smaand = "Mei";
                 sdagen = "31";
-                antwoord = "dit is het jaartal en de maand die bij uw nummer hoort en hoeveel dagen deze maand heeft: " + smaand + " en " + sdagen + " dagen.";
+                antwoord = "dit is het jaartal en de maand die bij uw nummers hoort en hoeveel dagen deze maand dan heeft: " + smaand + " " + sjaar + " en " + sdagen + " dagen.";
             }
             if (dmaand == 6) {
                 smaand = "Juni";
                 sdagen = "30";
-                antwoord = "dit is het jaartal en de maand die bij uw nummer hoort en hoeveel dagen deze maand heeft: " + smaand + " en " + sdagen + " dagen.";
+                antwoord = "dit is het jaartal en de maand die bij uw nummers hoort en hoeveel dagen deze maand dan heeft: " + smaand + " " + sjaar + " en " + sdagen + " dagen.";
             }
             if (dmaand == 7) {
                 smaand = "Juli";
                 sdagen = "31";
-                antwoord = "dit is het jaartal en de maand die bij uw nummer hoort en hoeveel dagen deze maand heeft: " + smaand + " en " + sdagen + " dagen.";
+                antwoord = "dit is het jaartal en de maand die bij uw nummers hoort en hoeveel dagen deze maand dan heeft: " + smaand + " " + sjaar + " en " + sdagen + " dagen.";
             }
             if (dmaand == 8) {
                 smaand = "Augustus";
                 sdagen = "31";
-                antwoord = "dit is het jaartal en de maand die bij uw nummer hoort en hoeveel dagen deze maand heeft: " + smaand + " en " + sdagen + " dagen.";
+                antwoord = "dit is het jaartal en de maand die bij uw nummers hoort en hoeveel dagen deze maand dan heeft: " + smaand + " " + sjaar + " en " + sdagen + " dagen.";
             }
             if (dmaand == 9) {
                 smaand = "September";
                 sdagen = "30";
-                antwoord = "dit is het jaartal en de maand die bij uw nummer hoort en hoeveel dagen deze maand heeft: " + smaand + " en " + sdagen + " dagen.";
+                antwoord = "dit is het jaartal en de maand die bij uw nummers hoort en hoeveel dagen deze maand dan heeft: " + smaand + " " + sjaar + " en " + sdagen + " dagen.";
             }
             if (dmaand == 10) {
                 smaand = "Oktober";
                 sdagen = "31";
-                antwoord = "dit is het jaartal en de maand die bij uw nummer hoort en hoeveel dagen deze maand heeft: " + smaand + " en " + sdagen + " dagen.";
+                antwoord = "dit is het jaartal en de maand die bij uw nummers hoort en hoeveel dagen deze maand dan heeft: " + smaand + " " + sjaar + " en " + sdagen + " dagen.";
             }
             if (dmaand == 11) {
                 smaand = "November";
                 sdagen = "30";
-                antwoord = "dit is het jaartal en de maand die bij uw nummer hoort en hoeveel dagen deze maand heeft: " + smaand + " en " + sdagen + " dagen.";
+                antwoord = "dit is het jaartal en de maand die bij uw nummers hoort en hoeveel dagen deze maand dan heeft: " + smaand + " " + sjaar + " en " + sdagen + " dagen.";
             }
             if (dmaand == 12) {
                 smaand = "December";
                 sdagen = "31";
-                antwoord = "dit is het jaartal en de maand die bij uw nummer hoort en hoeveel dagen deze maand heeft: " + smaand + " en " + sdagen + " dagen.";
+                antwoord = "dit is het jaartal en de maand die bij uw nummers hoort en hoeveel dagen deze maand dan heeft: " + smaand + " " + sjaar + " en " + sdagen + " dagen.";
             }
             if (dmaand >= 13){
                 antwoord = "dit nummer is geen geldige maand";
