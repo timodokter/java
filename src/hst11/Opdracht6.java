8f0b8f30433296c7fb8e02857050cff345208781
timodokter <timo.dokter@student.rocvf.nl>
2020-09-15 11:15:45 +0100

    update 11.7

--- src/hst11/Opdracht6.java
+++ src/hst11/Opdracht6.java
@@ -8,16 +8,16 @@ public class Opdracht6 extends Applet {
     public void paint(Graphics g) {
         int x = 50;
         int y = 50;
-        int h = 50;
-        int w = 50;
+        int h = 100;
+        int w = 100;
         int arcangle = 360;
         int arcstart = 0;
         int aantal = 0;
 
         while (aantal < 5) {
             aantal++;
-            h -= 10;
-            w -= 10;
+            h -= 20;
+            w -= 20;
             x += 10;
             y += 10;
             g.drawArc(x,y,w,h,arcstart,arcangle);
