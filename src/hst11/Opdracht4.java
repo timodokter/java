d7cb9583a16223a4bd42dbe836c69a8a2367d2a7
timodokter <timo.dokter@student.rocvf.nl>
2020-09-15 10:31:29 +0100

    update 11.4

--- src/hst11/Opdracht4.java
+++ src/hst11/Opdracht4.java
@@ -8,6 +8,15 @@ import java.awt.*;
 public class Opdracht4 extends Applet {
 
     public void paint(Graphics g) {
+        int getal = 0;
+        int x = 10;
+        int tafel;
 
+        while (getal < 10) {
+            x += 15;
+            getal++;
+            tafel = 3 * getal;
+            g.drawString("" + tafel,x,10);
+        }
     }
 }
