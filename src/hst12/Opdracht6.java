84b013c662701d982da4fd415e25a3207e4eccb8
timodokter <timo.dokter@student.rocvf.nl>
2020-09-17 11:57:32 +0100

    update 12.8

--- src/hst12/Opdracht6.java
+++ src/hst12/Opdracht6.java
@@ -1,7 +1,40 @@
 package hst12;
 
 import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Opdracht6 extends Applet {
+    //decleratie
+    double[] tabel = {4,2,1,4,3,2,3,3,4,2,5};
+    TextField tekstvak;
+    Label label;
+    int index;
+    int aantal;
 
+    public void init() {
+        label = new Label("type een getal tussen de 1 en de 5 en druk op enter");
+        tekstvak = new TextField("",5);
+        tekstvak.addActionListener(new listener());
+        add(label);
+        add(tekstvak);
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("het ingevulde nummer komt " +  "keer voor.",20,20);
+    }
+
+    private class listener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            aantal = 0;
+            index = Integer.parseInt(tekstvak.getText());
+            for (int i=0;i < tabel.length; i++) {
+                if (index == tabel[i]) {
+                    aantal++;
+                }
+            }
+            repaint();
+        }
+    }
 }
