692758a58a11c9be558c4cb47fb430127cd93f7e
timodokter <timo.dokter@student.rocvf.nl>
2020-09-17 10:10:13 +0100

    update 12.5

--- src/hst12/Opdracht4.java
+++ src/hst12/Opdracht4.java
@@ -1,9 +1,53 @@
 package hst12;
 
 import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Opdracht4 extends Applet {
     //decleratie
-    double[] getallen;
-    
+    boolean gevonden;
+    int[] getallen = {10,43,29,97,45,136,230,210,154};
+    TextField tekstvak;
+    Label label;
+    Button ok;
+    int gezocht;
+
+    public void init() {
+        //initialisatie
+        label = new Label("type een cijfer tussen de 1 en de 250");
+        tekstvak = new TextField("",5);
+        add(label);
+        add(tekstvak);
+        ok = new Button("oké");
+        ok.addActionListener(new listener());
+        gevonden = false;
+        add(ok);
+    }
+
+    public void paint(Graphics g) {
+        if (gevonden == true) {
+            g.drawString("De waarde is gevonden in de index",20,50);
+        }
+        else {
+            g.drawString("De waarde is niet gevonden in de index",20,50);
+        }
+    }
+
+    private class listener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            gevonden = false;
+            gezocht = Integer.parseInt(tekstvak.getText());
+
+            for (int i=0; i < getallen.length; i++) {
+                if (gezocht == getallen[i]) {
+                    gevonden = true;
+                    break;
+                }
+            }
+            repaint();
+        }
+    }
 }
+
