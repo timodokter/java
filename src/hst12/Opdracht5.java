670bbb041ada44ed5d50872bfb691f948c64d1c0
timodokter <timo.dokter@student.rocvf.nl>
2020-09-17 10:31:47 +0100

    update 12.6

---
+++ src/hst12/Opdracht5.java
@@ -0,0 +1,25 @@
+package hst12;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Opdracht5 extends Applet {
+    //decleratie
+    int[] getallen = {10,43,29,97,45,136,230,210,154};
+    int max;
+
+    public void init() {
+        max = getallen[0];
+        for (int teller = 0; teller < getallen.length; teller++) {
+            if (getallen[teller] > max) {
+                max = getallen[teller];
+            }
+        }
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("het maximum is: " + max,20,50);
+    }
+}
