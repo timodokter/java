7ffedcdb0907d3068fc14ee4e7fefedbf66ec5dd
timodokter <timo.dokter@student.rocvf.nl>
2020-09-23 12:05:13 +0100

    update 14.2

--- src/hst14/Opdracht1.java
+++ src/hst14/Opdracht1.java
@@ -4,7 +4,6 @@ import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.lang.management.ManagementFactory;
 
 public class Opdracht1 extends Applet {
 
