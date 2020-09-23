9912feb671918c5275c2fa44a28db3b2f7d768b9
timodokter <timo.dokter@student.rocvf.nl>
2020-09-23 15:55:12 +0100

    update 14.4

--- src/hst14/Opdracht2.java
+++ src/hst14/Opdracht2.java
@@ -4,12 +4,14 @@ import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class Opdracht2 extends Applet {
 
     //decleratie
     double kleurkiezer;
     double nummerkiezer;
+    String[] deck;
 
     String[] kleur = {"Ruiten", "Harten", "Schoppen", "Klaver"};
     String[] nummer = {"aas", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "tien", "Boer", "Vrouw", "Heer"};
@@ -17,14 +19,14 @@ public class Opdracht2 extends Applet {
     Button delen;
 
     private String deelkaart() {
-        int random = new random.nextInt(deck.length);
-        String kaart = deck[random];
+        int random = new Random.nextInt(deck.length);
+        String kaart = (deck[random]);
 
-        String[] hulplijst= new String[deck.legnth - 1];
+        String[] hulplijst= new String[deck.length - 1];
         int hulpindex = 0;
         for (int i = 0; i < deck.length; i++) {
             if (i != random) {
-                hulplijst[hulpindex] = deck[i];
+                hulplijst[hulpindex] = (deck[i]);
 
             }
         }
