7505c793c2a0baab187665de81d2e1ec339416c2
timodokter <timo.dokter@student.rocvf.nl>
2020-09-30 15:43:28 +0100

    opdracht stappenplan 1.1

--- src/opdrachtstappenplan/Opdracht.java
+++ src/opdrachtstappenplan/Opdracht.java
@@ -5,13 +5,20 @@ import java.applet.Applet;
 public class Opdracht extends Applet {
     //decleratie
     int getal;
+    int kwadraat;
 
     public void init() {
         //initialisatie
         getal = 13;
         if (getal >= 0 && getal == (int)getal) {
-
+            for (int x = 1; x <= getal; x++) {
+                if (x % 2 == 0) {
+                    if (x % 6 == 0) {
+                        kwadraat = x * x;
+                        System.out.println(kwadraat);
+                    }
+                }
+            }
         }
     }
-
 }
